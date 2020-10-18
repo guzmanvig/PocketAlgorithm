7c8395c2c12e9a196b94a29452445a3398faa4cc
Guzman <guzmanvigliecca@gmail.com>
2020-10-18 14:31:02 -0400

    all data sets

--- pocket/pocket_kernelized_algorithm.m
+++ pocket/pocket_kernelized_algorithm.m
@@ -1,21 +1,20 @@
-data = xlsread('debt.xls');
-% Test set as the first half of data
-test_set = data(1:floor(size(data, 1)/2), :);
-X_test = test_set(:,2:size(test_set, 2)-1); % removes first column (id) and last (class label)
-y_test = test_set(:,size(test_set, 2):size(test_set, 2)); %last column is the class label
-% Training set as the second half of data
-training_set = data((floor(size(data, 1)/2) + 1):end, :);
-X = training_set(:,2:size(training_set, 2)-1); % removes first column (id) and last (class label)
-y = training_set(:,size(training_set, 2):size(training_set, 2)); %last column is the class label
+% data = xlsread('debt.xls');
+% % Test set as the first half of data
+% test_set = data(1:floor(size(data, 1)/2), :);
+% X_test = test_set(:,2:size(test_set, 2)-1); % removes first column (id) and last (class label)
+% y_test = test_set(:,size(test_set, 2):size(test_set, 2)); %last column is the class label
+% % Training set as the second half of data
+% training_set = data((floor(size(data, 1)/2) + 1):end, :);
+% X = training_set(:,2:size(training_set, 2)-1); % removes first column (id) and last (class label)
+% y = training_set(:,size(training_set, 2):size(training_set, 2)); %last column is the class label
 
-% data = table2array(readtable('iris_modified.txt'));
-% suffled_data = data(randperm(size(data, 1)), :); % shuffled data
+% data = xlsread('iris_shuffled.xlsx');
 % % Test set as the first half of the shuffled data
-% test_set = suffled_data(1:size(suffled_data, 1)/2, :);
+% test_set = data(1:size(data, 1)/2, :);
 % X_test = test_set(:,1:size(test_set, 2)-1); % removes last column(class label)
 % y_test = test_set(:,size(test_set, 2):size(test_set, 2)); %last column is the class label
 % % Training set as the second half of the shuffled data
-% training_set = suffled_data((size(suffled_data, 1)/2 + 1):end, :);
+% training_set = data((size(data, 1)/2 + 1):end, :);
 % X = training_set(:,1:size(training_set, 2)-1); % removes last column(class label)
 % y = training_set(:,size(training_set, 2):size(training_set, 2)); %last column is the class label
 
@@ -31,6 +30,72 @@ y = training_set(:,size(training_set, 2):size(training_set, 2)); %last column is
 % y = training_set(:,size(training_set, 2):size(training_set, 2)); %last column is the class label
 % y = (y - 2) / 2; % transform 2 in 0 and 4 in 1
 
+% training_set = table2array(readtable('shuttle_training.txt'));
+% test_set = table2array(readtable('shuttle_test.txt'));
+% X_test = test_set(1:5000,1:size(test_set, 2)-1); % removes last column (class label)
+% y_test = xlsread('shuttle_y_test.xlsx');
+% X = training_set(1:5000,1:size(training_set, 2)-1); % removes first column (id) and last (class label)
+% y = xlsread('shuttle_y_training.xlsx');
+
+% data = xlsread('wholesale_data.csv');
+% % Test set as the first half of data
+% test_set = data(1:floor(size(data, 1)/2), :);
+% X_test = test_set(:,2:size(test_set, 2)); % removes first column(class label)
+% y_test = test_set(:,1); %first column is the class label
+% y_test = (y_test - 1); % transform 2 in 1 and 1 in 0
+% % Training set as the second half of data
+% training_set = data((floor(size(data, 1)/2) + 1):end, :);
+% X = training_set(:,2:size(test_set, 2)); % removes first column(class label)
+% y = training_set(:,1); %first column is the class label
+% y = (y - 1); % transform 2 in 1 and 1 in 0
+
+% data = xlsread('HTRU.csv');
+% % Test set as the first half of the shuffled data
+% test_set = data(1:size(data, 1)/2, :);
+% X_test = test_set(:,1:size(test_set, 2)-1); % removes last column(class label)
+% y_test = test_set(:,size(test_set, 2):size(test_set, 2)); %last column is the class label
+% % Training set as the second half of the shuffled data
+% training_set = data((size(data, 1)/2 + 1):end, :);
+% X = training_set(:,1:size(training_set, 2)-1); % removes last column(class label)
+% y = training_set(:,size(training_set, 2):size(training_set, 2)); %last column is the class label
+
+data = table2array(readtable('Skin_cut.txt'));
+% Test set as the first half of the shuffled data
+%test_set = data(1:size(data, 1)/2, :);
+test_set = data(1:5000, :);
+X_test = test_set(:,1:size(test_set, 2)-1); % removes last column(class label)
+y_test = test_set(:,size(test_set, 2):size(test_set, 2)); %last column is the class label
+y_test = (y_test - 1); % transform 2 in 1 and 1 in 0
+% Training set as the second half of the shuffled data
+%training_set = data((size(data, 1)/2 + 1):end, :);
+training_set = data(5001:10000, :);
+X = training_set(:,1:size(training_set, 2)-1); % removes last column(class label)
+y = training_set(:,size(training_set, 2):size(training_set, 2)); %last column is the class label
+y = (y - 1); % transform 2 in 1 and 1 in 0
+
+% data = xlsread('position_modified_cut.xlsx');
+% % Test set as the first half of the shuffled data
+% %test_set = data(1:size(data, 1)/2, :);
+% test_set = data(1:5000, :);
+% X_test = test_set(:,1:size(test_set, 2)-1); % removes last column(class label)
+% y_test = test_set(:,size(test_set, 2):size(test_set, 2)); %last column is the class label
+% % Training set as the second half of the shuffled data
+% %training_set = data((size(data, 1)/2 + 1):end, :);
+% training_set = data(5001:10000, :);
+% X = training_set(:,1:size(training_set, 2)-1); % removes last column(class label)
+% y = training_set(:,size(training_set, 2):size(training_set, 2)); %last column is the class label
+
+% data = xlsread('abalone_01_shuffled.xlsx');
+% %suffled_data = data(randperm(size(data, 1)), :);
+% % Test set as the first half of the shuffled data
+% test_set = data(1:size(data, 1)/2, :);
+% X_test = test_set(:,1:size(test_set, 2)-1); % removes last column(class label)
+% y_test = test_set(:,size(test_set, 2):size(test_set, 2)); %last column is the class label
+% % Training set as the second half of the shuffled data
+% training_set = data((size(data, 1)/2 + 1):end, :);
+% X = training_set(:,1:size(training_set, 2)-1); % removes last column(class label)
+% y = training_set(:,size(training_set, 2):size(training_set, 2)); %last column is the class label
+
 
 X = [ones(size(X, 1), 1) X]; % add column of ones
 y = 2*y - 1;  % transform 0 in -1 
@@ -42,12 +107,12 @@ kernel = "COSINE";
 alpha = zeros(size(X,1), 1); % number of times each data point was missclasified
 
 % initialize stuff
-max_step = 10 * size(X,1); % maximum number of iterations
+max_step = 5*size(X, 1); % maximum number of iterations
 eta = 1;         % the coefficient for the update rule (0 < eta <= 1)
 step = 1;
 run = 0;
 best_run = 0;
-max_run = 2 * size(X, 1);
+max_run = size(X, 1);
 alpha_pocket = alpha;
 
 
